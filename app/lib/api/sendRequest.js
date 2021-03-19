4da9e8374cb9af4e1c9133377a641fac4d8bfc21
kacoulib <kacoulib@gmail.com>
2021-03-19 02:37:50 +0100

    Removed console

--- app/lib/api/sendRequest.js
+++ app/lib/api/sendRequest.js
@@ -2,7 +2,7 @@ import 'isomorphic-unfetch';
 import { toast } from 'react-toastify';
 
 const { NEXT_PUBLIC_ROOT_URL } = require('../../config');
-console.log(process.env)
+
 export default async function sendRequest(path, options = {}, extra) {
   const headers = {
     ...(options.headers || {}),
