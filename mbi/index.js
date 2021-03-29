0853ae43d13aaa30e4332fff086f0a17c89a62ff
kacoulib <kacoulib@gmail.com>
2021-03-30 00:23:14 +0200

    Added cron

--- mbi/index.js
+++ mbi/index.js
@@ -1,17 +1,13 @@
-const fs = require('fs')
+const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const CronJob = require('cron').CronJob
-const {
-  MONGO_URL,
-  PORT,
-  ROOT_URL,
-} = require('./config');
+const CronJob = require('cron').CronJob;
+const { MONGO_URL, PORT, ROOT_URL } = require('./config');
 const readMbi = require('./utils/mbi');
 const ftp = require('./utils/ftp');
 const logger = require('./logs');
-const app = express()
+const app = express();
 const options = {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -22,14 +18,18 @@ mongoose.connect(MONGO_URL, options);
 app.use(helmet());
 app.use(express.static('public'));
 
-// const job = new CronJob('0 0 */6 * * *', function() {
-//   const d = new Date();
-//   logger.log('Starting mbi at sync', d)
-  // ftp(readMbi);
-  // readMbi();
-// });
-// job.start();
+const job = new CronJob('0 0 */6 * * *', function () {
+  try {
+    const d = new Date();
+    logger.log('Starting mbi at sync', d);
+    ftp(readMbi);
+    // readMbi();
+  } catch (err) {
+    logger.error(err);
+  }
+});
+job.start();
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at ${ROOT_URL}`)
-})
+  console.log(`Example app listening at ${ROOT_URL}`);
+});
