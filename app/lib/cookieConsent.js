9e3178f600fd85f86ed7b56eb90993edef956709
kacoulib <kacoulib@gmail.com>
2021-04-02 02:58:50 +0200

    Fixed cookieConsent

--- app/lib/cookieConsent.js
+++ app/lib/cookieConsent.js
@@ -1,20 +1,23 @@
 import { DidomiSDK } from '@didomi/react';
 
 const didomiConfig = {
-    app: {
-      vendors: {
-        iab: {
-          all: true,
-        },
+  app: {
+    vendors: {
+      iab: {
+        all: true,
       },
     },
-  };
-export default ()=> (
-    <DidomiSDK
+  },
+  notice: {
+    position: 'bottom',
+  },
+};
+export default () => (
+  <DidomiSDK
     config={didomiConfig}
     apiKey="923bcadb-dc92-44c8-b6eb-1dc9fe53085d"
     iabVersion={2}
     sdkPath="https://sdk.privacy-center.org/"
     gdprAppliesGlobally
   />
-)
+);
