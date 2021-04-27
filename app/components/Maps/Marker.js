601c382248437afdf7e1189a2a85885f7fce4c86
kacoulib <kacoulib@gmail.com>
2021-04-27 17:02:26 +0200

    Fixed maps marker

--- app/components/Maps/Marker.js
+++ app/components/Maps/Marker.js
@@ -3,21 +3,21 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import { MapsCard } from 'components/card';
-import CustomMarker from '../../static/img/icons/customMarker.svg';
-import SelectedCustomMarker from '../../static/img/icons/selectedCustomMarker.svg';
+import WhiteMarker from 'static/img/white_marker.png';
+import BlueMarker from 'static/img/blue_marker.png';
 import { styles } from './styles';
 
 const useStyles = makeStyles(styles);
 const Marker = ({ data, show, isMobile, inGroup, handleBookmark, liked }) => {
   const classes = useStyles({ isTop: data?.isTop, isLeft: data?.isLeft });
   let className = inGroup ? classes.markerInGroupStyled : classes.markerStyled;
-  const Icon = show ? SelectedCustomMarker : CustomMarker;
+  const src = !show ? WhiteMarker : BlueMarker;
   className = clsx(className, classes.markerPosition);
 
   return (
     <div>
       <div className={className}>
-        <Icon className={classes.markerIcon} />
+        <img src={src} className={classes.markerIcon} />
         {show && !isMobile && data?.showInfoWindow && (
           <MapsCard
             {...data}
