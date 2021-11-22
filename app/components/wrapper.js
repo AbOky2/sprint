1460d2552b804fc7063a6a75dbd3d8593d25dd89
Issa <issa@iMacPrint-StartUp-Studio.local>
2021-11-22 14:01:33 +0100

    commit pour la redirection 2

--- app/components/wrapper.js
+++ app/components/wrapper.js
@@ -80,9 +80,8 @@ const AdminContentWrapper = withStyles(styles)(
           <div
             className={classes.redirectStyle}
             onClick={(e) => {
-              //if (href) router.push(href);
-              //else router.back();
-              router.back();
+              if (href) router.push(href);
+              else router.back();
             }}
           >
             <Typography variant="h4">Retour</Typography>
