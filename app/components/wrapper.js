2a1017ab04184b951d62ae83a71eb2300473fae8
Issa <issa@iMacPrint-StartUp-Studio.local>
2021-11-23 15:36:02 +0100

    on continue toujours avec la redir

--- app/components/wrapper.js
+++ app/components/wrapper.js
@@ -80,8 +80,7 @@ const AdminContentWrapper = withStyles(styles)(
           <div
             className={classes.redirectStyle}
             onClick={(e) => {
-              if (href) router.push("http://kitlenid.fr");
-              else router.back();
+              router.push("http://kitlenid.fr");
             }}
           >
             <Typography variant="h4">Retour</Typography>
