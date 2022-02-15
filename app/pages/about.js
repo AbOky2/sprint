6c50fe4234fa3fc6e2eb846ce26d8e82956af634
Yosko Issa oki <yoskoissaoki@MacBook-Pro-de-Yosko.local>
2022-02-15 15:03:22 +0100

    commit

---
+++ app/pages/about.js
@@ -0,0 +1,138 @@
+
+import Footer from 'components/Footer/Footer'
+import Header from 'components/Header/Header'
+import React from 'react'
+
+export default function About() {
+    return (
+        <>
+      <Header />
+    <section
+      className="
+        firmm4_testimonails1
+        carousel
+        slide
+        testimonials-slider
+        cid-sAFYKGLBO2
+      "
+      data-interval="false"
+      id="testimonials1-1f"
+    >
+      <div className="text-center container">
+        <div className="row">
+          <div className="col-lg-11 col-md-8 col-sm-10 col-12">
+            <div
+              className="carousel slide"
+              role="listbox"
+              data-pause="true"
+              data-keyboard="false"
+              data-ride="false"
+              data-interval="false"
+            >
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <div className="col col-12 user active">
+                    <div className="user_text">
+                      <p className="mbr-fonts-style mb-0 display-4">
+                        <strong
+                          ><em
+                            >“Et pourquoi l’achat immobilier ne serait-il pas
+                            accessible aux jeunes ?”<br /></em></strong
+                        >Nous avons créé Kit le nid au sein du groupe immobilier
+                        Nexity pour aider les jeunes à devenir propriétaire.
+                        Aucun acteur ne s’adresse à eux et pourtant leur intérêt
+                        pour l’achat immobilier est croissant.<br /><br /><strong
+                          >Notre objectif : <br /></strong
+                        >Les accompagner dans cette étape en simplifiant et en
+                        digitalisant le parcours d’achat.<br />
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <ol className="carousel-indicators">
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="0"
+                  className="active"
+                ></li>
+              </ol>
+                        <div className="carousel-controls">
+                <a className="carousel-control-prev" role="button" data-slide="prev">
+                    <span aria-hidden="true" className="mobi-mbri mobi-mbri-arrow-prev mbr-iconfont"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+
+                <a className="carousel-control-next" role="button" data-slide="next">
+                    <span aria-hidden="true" className="mobi-mbri mobi-mbri-arrow-next mbr-iconfont"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section className="tabs firmm4_tabs2 cid-sAFYeaVi4F" id="tabs2-1e">
+      <div className="container">
+        <div className="row"></div>
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-12">
+            <div className="tab-content">
+              <div id="tab1" className="tab-pane in active" role="tabpanel">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="image">
+                      <img src="assets/images/img-5786-1-1222x763.png" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div id="tab2" className="tab-pane" role="tabpanel">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="image">
+                      <img src="assets/images/firm8.jpg" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div id="tab3" className="tab-pane" role="tabpanel">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="image">
+                      <img src="assets/images/firm6.jpg" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div id="tab4" className="tab-pane" role="tabpanel">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="image">
+                      <img src="assets/images/firm9.jpg" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div id="tab5" className="tab-pane" role="tabpanel">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="image">
+                      <img src="assets/images/firm11.jpg" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<Footer />
+
+        </>
+    )
+}
